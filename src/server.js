067b6f171f792e1034c1b9959bc22a7c7151e1b4
Peter Neely <pgneely@gmail.com>
2018-04-02 19:32:40 -0700

    heroku deployment requirements

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+  console.log('listening on port', server.address().port);
 });
